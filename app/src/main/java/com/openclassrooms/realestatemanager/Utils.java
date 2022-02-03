3bb71fff1d4b029a8a9b1f91e472c8f1b133d005
Alexandre de Boursac <alexdb.freelance@gmail.com>
2022-02-03 18:28:23 +0100

    Utils methods

--- app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
@@ -3,9 +3,8 @@ package com.openclassrooms.realestatemanager;
 import android.content.Context;
 import android.net.wifi.WifiManager;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by Philippe on 21/02/2018.
@@ -14,33 +13,53 @@ import java.util.Date;
 public class Utils {
 
     /**
-     * Conversion d'un prix d'un bien immobilier (Dollars vers Euros)
-     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
-     * @param dollars
-     * @return
+     * Conversion of a price of a property (Dollars to Euros)
+     *
+     * @param dollars amount in dollars to be converted
+     * @return amount converted into dollars
      */
-    public static int convertDollarToEuro(int dollars){
+    public static int convertDollarToEuro(int dollars) {
         return (int) Math.round(dollars * 0.812);
     }
 
     /**
-     * Conversion de la date d'aujourd'hui en un format plus approprié
-     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
-     * @return
+     * Conversion of a price of a property (Euros to Dollars)
+     *
+     * @param euro amount in euros to be converted
+     * @return amount converted into euros
      */
-    public static String getTodayDate(){
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-        return dateFormat.format(new Date());
+    public static int convertEuroToDollar(int euro) {
+        return (int) Math.round((float) euro / 0.812);
     }
 
     /**
-     * Vérification de la connexion réseau
-     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
-     * @param context
-     * @return
+     * Converting today's date to a more appropriate format
+     *
+     * @return date in dd/MM/yyyy format
      */
-    public static Boolean isInternetAvailable(Context context){
-        WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+    public static String getTodayDate() {
+        return LocalDate.now().format(getDefaultTimeFormatter());
+    }
+
+    /**
+     * Network connection check
+     *
+     * @param context Context
+     * @return true if wifi is enabled, false instead
+     */
+    public static Boolean isInternetAvailable(Context context) {
+        Context applicationContext = context.getApplicationContext();
+        WifiManager wifi = (WifiManager) applicationContext.getSystemService(Context.WIFI_SERVICE);
+        if (wifi == null) return false;
         return wifi.isWifiEnabled();
+    }
+
+    /**
+     * Provide a default date format in dd/MM/yyyy format
+     *
+     * @return Date
+     */
+    public static DateTimeFormatter getDefaultTimeFormatter() {
+        return DateTimeFormatter.ofPattern("dd/MM/yyyy");
     }
 }
