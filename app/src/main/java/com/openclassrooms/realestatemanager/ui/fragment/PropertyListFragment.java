b59285ae7a5180b7f65cc372dac84ba98d948f9b
Alexandre de Boursac <alexdb.freelance@gmail.com>
2022-02-25 22:08:55 +0100

    Improve UI design

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/fragment/PropertyListFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/fragment/PropertyListFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -54,6 +55,7 @@ public class PropertyListFragment extends Fragment implements CommandSelectPrope
 
         PropertyListAdapter mAdapter = new PropertyListAdapter(mProperties, this);
         mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
     }
 
     private void initObservers() {
